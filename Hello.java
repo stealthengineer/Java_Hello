249e494be7698d7eafbe0d159399adf9191e5382
stealthengineer <jungmindl@gmail.com>
2025-02-17 00:50:15 -0800

    Second commit

--- Hello.java
+++ Hello.java
@@ -1,10 +1,10 @@
 class Hello{
-public static void main(String a[])
+public static void main(String args[])
 {
-    int num1 = 3;
-    int num2 = 5;
-    int result = num1 + num2;
-    System.out.println(result);
+   int num1 = 7;
+   int num2 = 5;
 
+   int result = num1 + num2;
+   System.out.println(result);
 }
 }
